b68e69d32b670d892c22ab0226dca6d9f96511f5
Ruben Niculcea <ruben.niculcea@gmail.com>
2018-02-12 21:43:06 -0500

    Do not render the footer spacer

--- src/index.js
+++ src/index.js
@@ -87,7 +87,7 @@ class ParallaxScrollView extends Component {
     const background = this._renderBackground({ fadeOutBackground, backgroundScrollSpeed, backgroundColor, parallaxHeaderHeight, stickyHeaderHeight, renderBackground });
     const foreground = this._renderForeground({ fadeOutForeground, parallaxHeaderHeight, stickyHeaderHeight, renderForeground: renderForeground || renderParallaxHeader });
     const bodyComponent = this._wrapChildren(children, { contentBackgroundColor, stickyHeaderHeight, contentContainerStyle });
-    const footerSpacer = this._renderFooterSpacer({ contentBackgroundColor });
+    //const footerSpacer = this._renderFooterSpacer({ contentBackgroundColor });
     const maybeStickyHeader = this._maybeRenderStickyHeader({ parallaxHeaderHeight, stickyHeaderHeight, backgroundColor, renderFixedHeader, renderStickyHeader });
     const scrollElement = renderScrollComponent(scrollViewProps);
 
@@ -104,7 +104,7 @@ class ParallaxScrollView extends Component {
             },
             foreground,
             bodyComponent,
-            footerSpacer
+            //footerSpacer
           )
         }
         { maybeStickyHeader }
